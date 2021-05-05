b56a67048eff32fb69fc3a21ec79a1f807ec464d
1ch1zoku <nkhomin666@gmail.com>
2021-05-05 16:35:45 +0300

    Init

---
+++ front/src/storage.js
@@ -0,0 +1,38 @@
+const storage = {
+    photo:[{
+        id:1,
+       author:"Вася",
+       name:"Машина",
+       description:"Гоночний автомобіль",
+       url:"https://cdn.shazoo.ru/c800x360/424131_iC8mZhz0Oy_qxtyaa3qxvbu5nukposgeybd6m.jpg",
+       hashtag:"#гонка #швидкість #машина",
+       date:(2000,3,2),
+       count_like:2383,
+       list_comment:["Швидкість","Супер"]
+      },
+      {
+        id:2,
+        author:"Банан",
+        name:"Банан",
+        description:"Банан",
+        url:"https://nashzelenyimir.ru/wp-content/uploads/2016/07/%D0%91%D0%B0%D0%BD%D0%B0%D0%BD-%D1%84%D0%BE%D1%82%D0%BE.jpg",
+        hashtag:"#банан #БАНАН",
+        date:(2011,11,11),
+        count_like:64837,
+        list_comment:["НАНАБ"]
+      },
+      {
+        id:3,
+        author:"Анонім",
+        name:"Електроніка",
+        description:"З+4=6",
+        url:"https://finik.me/media/article_images/paradox.jpg",
+        hashtag:"#6666666",
+        date:(2020,3,6),
+        count_like:29856,
+        list_comment:["6"]
+      }]
+
+}
+
+export default storage;
